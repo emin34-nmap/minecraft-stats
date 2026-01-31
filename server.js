2239395e027e448c1a6f3a57c53abbf6a2566c11
emin34-nmap <emin.cpvp@gmail.com>
2026-01-31 14:23:03 +0300

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static("public")); // Public klasörünü dışarı aç
-
-app.listen(3000, () => {
-  console.log("Server çalışıyor: http://localhost:3000");
-});
